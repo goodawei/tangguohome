5a417ba2fe3b442d4891dc1907ed1d23bd431937
bjletaoba@sina.com <bjletaoba@sina.com>
2017-07-29 16:04:05 +0800

    m site init

---
+++ app/Http/Controllers/Wap/ActivityController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers\Wap;
+use App\Http\Controllers\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: lihongwei
+ * Date: 17/7/29
+ * Time: 下午3:55
+ */
+class ActivityController extends Controller
+{
+    public function index()
+    {
+        dd('m site is ok');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,41 @@
 |
 */
 
-Route::get('/', 'QuestionsController@index');
-//github webhook 通知
-Route::any('/deploy','DeployController@deploy');
-Route::any('test','TestController@test');
+Route::group(['domain'=>env('PC_SITE')],function(){
+    Route::get('/', 'QuestionsController@index');
+    //github webhook 通知
+    Route::any('/deploy','DeployController@deploy');
+    Route::any('test','TestController@test');
 
-Auth::routes();
+    Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+    Route::get('/home', 'HomeController@index');
 
-Route::get('email/verify/{token}',['as'=>'email.verify','uses'=>'EmailActiveController@verify']);
+    Route::get('email/verify/{token}',['as'=>'email.verify','uses'=>'EmailActiveController@verify']);
 
-Route::resource('questions','QuestionsController',['names'=>[
-    'create'=>'question.create',
-    'show'=>'question.show'
-]]);
+    Route::resource('questions','QuestionsController',['names'=>[
+        'create'=>'question.create',
+        'show'=>'question.show'
+    ]]);
 
-Route::post('questions/{question}/answer','AnswersController@store');
+    Route::post('questions/{question}/answer','AnswersController@store');
 
-Route::get('questions/{question}/follow','QuestionFollowController@follow');
+    Route::get('questions/{question}/follow','QuestionFollowController@follow');
 
-//专题
-Route::get('shabantang','SpecialtopicsController@index');
+    //专题
+    Route::get('shabantang','SpecialtopicsController@index');
 
 
-//flowmedia 测试
-Route::get('flow','TestController@media');
-//oauth 练习
-Route::get('oauth','OauthController@goTanguoServer');
-Route::get('getCode','OauthController@receiveCode');
-Route::get('yar','TestController@yar');
-Route::get('yar/handle','YarController@handle');
+    //flowmedia 测试
+    Route::get('flow','TestController@media');
+    //oauth 练习
+    Route::get('oauth','OauthController@goTanguoServer');
+    Route::get('getCode','OauthController@receiveCode');
+    Route::get('yar','TestController@yar');
+    Route::get('yar/handle','YarController@handle');
+});
+
+Route::group(['domain'=>env('M_SITE'),'namespace' => 'Wap'],function(){
+    Route::get('/', 'ActivityController@index');
+});
+
